20a1d0d8c72aa525861b3e9150f5dfc799b8855a
Kaede <kidhaibara@gmail.com>
2018-08-27 18:44:03 +0800

    Update plugin

--- plugin/src/main/java/com/kaedea/xml2java/Xml2JavaAction.java
+++ plugin/src/main/java/com/kaedea/xml2java/Xml2JavaAction.java
@@ -2,7 +2,13 @@ package com.kaedea.xml2java;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import org.apache.http.util.TextUtils;
 
 /**
  * @author Kaede
@@ -12,7 +18,22 @@ public class Xml2JavaAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        alert("Hello, xml2java");
+        // Get all the required data from data keys
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
+        // Access document, caret, and selection
+        final Document document = editor.getDocument();
+        final SelectionModel model = editor.getSelectionModel();
+
+        String text = model.getSelectedText();
+        if (TextUtils.isEmpty(text)) {
+            alert("Empty selected text.");
+            return;
+        }
+
+        Xml2Java.convert(text, codes -> {
+            alert(codes);
+        });
     }
 
     private void alert(String msg) {
