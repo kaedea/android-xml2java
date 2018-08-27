30c7ee8e3bc6289d4555a4be76ac6f4112b15878
Kaede <kidhaibara@gmail.com>
2018-08-27 17:56:15 +0800

    Initialing

---
+++ xml2java/src/test/java/com/kaedea/xml2java/ConvertTest.java
@@ -0,0 +1,49 @@
+package com.kaedea.xml2java;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.File;
+
+/**
+ * @author Kaede.
+ * @version 2018/8/27.
+ */
+@RunWith(JUnit4.class)
+public class ConvertTest {
+
+    @Test
+    public void xmlWithFile() {
+        final String[] java = {null};
+        File xmlFile = new File("/Users/Kaede/Repository/Repos/X2C/app/src/main/res/layout/activity_main.xml");
+
+        Xml2Java.convert(xmlFile, codes -> {
+            java[0] = codes;
+        });
+
+        Assert.assertNotNull(java);
+    }
+
+    @Test
+    public void xmlWithString() {
+        final String[] java = {null};
+
+        Xml2Java.convert(
+                "<ImageView\n" +
+                "android:id=\"@+id/iv\"\n" +
+                "android:layout_width=\"0dp\"\n" +
+                "android:layout_height=\"0dp\"\n" +
+                "android:scaleType=\"centerCrop\"\n" +
+                "android:src=\"@drawable/head\"\n" +
+                "app:layout_constraintDimensionRatio=\"H,1024:539\"\n" +
+                "app:layout_constraintLeft_toLeftOf=\"parent\"\n" +
+                "app:layout_constraintRight_toRightOf=\"parent\"\n" +
+                "app:layout_constraintTop_toTopOf=\"parent\" />", codes -> {
+            java[0] = codes;
+        });
+
+        Assert.assertNotNull(java);
+    }
+}
